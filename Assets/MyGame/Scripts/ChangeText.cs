a0200acae28f58200fafcd0d0126cb207e1c2147
Marc Leitner <marc.leitner@htl-salzburg.ac.at>
2019-05-20 15:46:41 +0200

    added first background in scene1 minigame

---
+++ Assets/MyGame/Scripts/ChangeText.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class ChangeText : MonoBehaviour {
+
+
+    public Text myText;
+    public TextMeshProUGUI myTMPText;
+    private int countPresses = 0;
+
+    public TestingRandom myScript;
+
+
+    private System.Random rnd = new System.Random();
+    private System.Random seedRnd = new System.Random(123);
+
+
+
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        myText.text = "Halooooooooooo";
+
+        myTMPText.text = "Nochmaaaalll jalllloooo";
+
+        Debug.Log("+++++++++"+ rnd.Next(0, 5));
+        Debug.Log("seed" + seedRnd.Next(0, 5));
+        Debug.Log("Random UE" + UnityEngine.Random.Range(0,1));
+
+
+
+    }
+	
+
+
+
+    public void addPress()
+    {
+        countPresses++;
+        // countPresses = countPresses + 1 , ist das selbe wie oben 
+        //countPresses += 1; same here
+
+        Debug.Log("counts: " + countPresses);
+        myText.text = countPresses.ToString();
+
+        Debug.Log("seed" + seedRnd.Next(0, 5));
+
+        myScript.ChangeRandomCube();
+
+
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
