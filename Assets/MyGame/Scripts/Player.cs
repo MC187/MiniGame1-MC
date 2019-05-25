65d4df3431e1935742dba33da98183c73255ec31
Marc Leitner <marc.leitner@htl-salzburg.ac.at>
2019-05-25 20:58:32 +0200

    added player + script, player now movable with arrow keys, tried collectible icecream with script  didnt work

---
+++ Assets/MyGame/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private const string AXISHORIZONTAL = "Horizontal";
+    private float moveSpeed = 6.5f;
+
+
+
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis(AXISHORIZONTAL) * Time.deltaTime * moveSpeed;
+        var newPosX = Mathf.Clamp(transform.position.x + deltaX, -300f, 300f);
+
+
+        transform.position = new Vector2(newPosX, transform.position.y);
+    }
+
+
+
+    void Update()
+    {
+
+        Move();
+    }
+}
